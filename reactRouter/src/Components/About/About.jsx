777e10476e97b7be6f807b4e4bde1614c3fcb6c6
prince-chaurasiya <princechaurasia2015@gmail.com>
2024-03-05 12:50:54 +0530

    react Router

--- reactRouter/src/Components/About/About.jsx
+++ reactRouter/src/Components/About/About.jsx
@@ -16,7 +16,7 @@ export default function About() {
                           React development is carried out by passionate developers
                       </h2>
                       <p className="mt-6 text-gray-600">
-                          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem
+                          Lorem ipsum dolor, amet consectetur adipisicing elit. Eum omnis voluptatem
                           accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde
                           aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!
                       </p>
